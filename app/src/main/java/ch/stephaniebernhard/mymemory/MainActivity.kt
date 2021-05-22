1d0969768eb91beadef50e3043b6e91fdef9bd17
stephaniebernhard <fanny.bernhard@gmail.com>
2021-05-22 17:25:28 +0200

    add Snackbar info for invalid moves

--- app/src/main/java/ch/stephaniebernhard/mymemory/MainActivity.kt
+++ app/src/main/java/ch/stephaniebernhard/mymemory/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ch.stephaniebernhard.mymemory.models.BoardSize
 import ch.stephaniebernhard.mymemory.models.MemoryCard
 import ch.stephaniebernhard.mymemory.models.MemoryGame
 import ch.stephaniebernhard.mymemory.utils.DEFAULT_ICONS
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,19 +19,20 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
-
+    private lateinit var clRoot: ConstraintLayout
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
 
     private lateinit var memoryGame: MemoryGame
     private lateinit var adapter: MemoryBoardAdapter
-    private var boardSize: BoardSize = BoardSize.HARD
+    private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot = findViewById(R.id.clRoot)
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
@@ -46,6 +49,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateGameWithFlip(position: Int) {
+        // Error checking
+        if (memoryGame.haveWonGame()) {
+            // Alert user of invalid move
+            Snackbar.make(clRoot, "You alreay won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if (memoryGame.isCardFaceUp(position)){
+            // Alert user of invalid move
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+
         memoryGame.flipCard(position)
         adapter.notifyDataSetChanged()
     }
--- app/src/main/java/ch/stephaniebernhard/mymemory/models/MemoryGame.kt
+++ app/src/main/java/ch/stephaniebernhard/mymemory/models/MemoryGame.kt
@@ -53,4 +53,12 @@ class MemoryGame(private val boardSize: BoardSize){
             }
         }
     }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFound == boardSize.getNumPairs()
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp
+    }
 }
